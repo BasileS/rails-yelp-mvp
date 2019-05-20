507946359ab4e5ab597f9802f495839855ec87a2
Basile Sowka <basile.sowka@gmail.com>
2019-05-20 17:02:17 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '0672491700',
+    category: 'italian'
+  },
+  {
+    name:         'Fils',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '0672491700',
+    category: 'chinese'
+  },
+  {
+    name:         'Hello',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '0672491700',
+    category: 'italian'
+  },
+  {
+    name:         'Alicheur',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '0672491700',
+    category: 'belgian'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '0672491700',
+    category: 'italian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
